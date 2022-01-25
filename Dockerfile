040b8c181e53cb96f084e8d6e23b99479a231730
binjie chen <binjie09@gmail.com>
2022-01-26 00:41:10 +0800

    add cache

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ ADD . /app/
 WORKDIR /app
 RUN npm install
 EXPOSE 3000
+ENV eth=0x4c8ee30c603c903241e1840ab721b3b358d3c2af
 #程序启动脚本
 CMD ["npm", "run", "prod"]
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import Koa from 'koa'
 import axios from 'axios';
 
 const app = new Koa();
+let res = { data: {} };
+const fetchData = async () => await axios.get(`https://k1pool.com/api/miner/eth/${process.env.eth}`);
+fetchData();
+setInterval(fetchData, 60000);
 
 app.use(async ctx => {
-  const res = await axios.get(`https://k1pool.com/api/miner/eth/${process.env.eth}`);
-  // https://k1ppoool.com/api/miner/eth/0x4c8ee30c603c903241e1840ab721b3b358d3c2af
-  ctx.body = `workers_online ${res.data.miner.workersOnline}
-avgHashrate ${res.data.miner.avgHashrate}
-curHashrate ${res.data.miner.curHashrate}
+  ctx.body = `workers_online ${res.data?.miner?.workersOnline}
+avgHashrate ${res.data?.miner?.avgHashrate}
+curHashrate ${res.data?.miner?.curHashrate}
 `;
 });
 
